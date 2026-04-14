833d98db20d64e6e578ae8ac484448c176b2bd41
Spouik <spouik.second@gmail.com>
2026-04-15 01:25:33 +0200

    clean up

--- source/program/main.cpp
+++ source/program/main.cpp
@@ -3,22 +3,11 @@
 // autosave offset for ver 2.0.2
 static constexpr ptrdiff_t AUTOSAVE_OFFSET = 0x92D0A8;
 
-// hook declaration
-/*HOOK_DEFINE_INLINE(DisableAutosaveHook) {
-    static void Callback(exl::hook::nx64::InlineCtx* ctx) {
-        // OG  : mov w8,#0x1
-        // new : mov w8,#0x0
-        ctx->W[8] = 0;
-    }
-};*/
 
 extern "C" void exl_main(void* x0, void* x1) {
     exl::hook::Initialize();
 
     exl::patch::CodePatcher patcher(AUTOSAVE_OFFSET);
 
-    // Applique le patch
     patcher.Write(0x3903E27F);
-
-    //DisableAutosaveHook::InstallAtOffset(AUTOSAVE_OFFSET);
 }
